f200e48a002472750d7b729ad419a1b4f40babc8
Jayashree <pedini14sneha@gmail.com>
2024-11-16 18:04:12 +0530

    feature/card-component: added reusable card component in t-card folder

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,4 +1,5 @@
 // export * from './lib';
-export * from './lib/t-main-layout'
+export * from './lib/t-main-layout';
+export * from './lib/t-card';
 // export * from './lib/ui';
 // export * from './lib/tn-button';
