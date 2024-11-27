fab08055e4d28743ea685c3a5d993a43c4f7fa41
rajatirole <Raja.Tirole@owens-minor.com>
2024-11-27 12:50:26 +0530

    feat(loginSignUp): added login signup component

---
+++ libs/ui/src/lib/pages/login-sign-up/login-sign-up.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { TButton } from '../../t-button'
+
+const LoginSignUp = () => {
+ return <div>LoginSignUp make changes <TButton text='login' ></TButton></div>
+}
+
+export default LoginSignUp
