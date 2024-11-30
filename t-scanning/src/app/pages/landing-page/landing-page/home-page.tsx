28a6c6441ae55d8c001f4ecb68611f27b17f4a4a
rajatirole <Raja.Tirole@owens-minor.com>
2024-11-30 16:44:55 +0530

    feat(review): add review and footer page

--- t-scanning/src/app/pages/landing-page/landing-page/home-page.tsx
+++ t-scanning/src/app/pages/landing-page/landing-page/home-page.tsx
@@ -1,15 +1,17 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import ReviewPage from '../../review-page/review-page'
+import FaqPage from '../../faq-page/faq-page'
+import FooterPage from '../../footer-page/footer-page'
 
 const HomePage = () => {
   return (
    <Box>
       {/* add top navigation bar here */}
     <div>nav bar</div>
-     {/* add footer here */}
-    <footer>footer should be here</footer>
-
-
+    <ReviewPage/>
+    <FaqPage/>
+    <FooterPage/>
 <Box>bottom bar</Box>
    </Box>
   )
