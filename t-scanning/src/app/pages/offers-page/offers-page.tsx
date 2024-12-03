2e622a00e74800c4cbc6a2565b910d80665d24fc
jayashreePedini <jayashree14work@gmail.com>
2024-12-03 12:13:58 +0530

    Update Offers Section Styling

--- t-scanning/src/app/pages/offers-page/offers-page.tsx
+++ t-scanning/src/app/pages/offers-page/offers-page.tsx
@@ -1,18 +1,25 @@
 import { Box, Typography } from '@mui/material';
 import { cardData } from '../../constants/LandingPageData';
-import { TCard } from '@tap-n-taste/ui';
+import { TButton, TCard } from '@tap-n-taste/ui';
 import '../../style.css';
 
 const OfferPage = () => {
   return (
     <Box className="mt-10 mb-10">
-      <Typography
-        variant="h5"
-        sx={{ fontFamily: 'Poppins', fontWeight: 'bold' }}
-      >
-        Offers of the day
-      </Typography>
-      <Box className="flex flex-row gap-10 overflow-x-auto no-scrollbar mt-3">
+      <Box className="flex justify-between items-center">
+        <Typography
+          variant="h5"
+          sx={{ fontFamily: 'Poppins', fontWeight: 'bold' }}
+        >
+          Offers of the day
+        </Typography>
+        <TButton
+          text="View All"
+          className={{ root: 'hover:bg-none', text: 'capitalize' }}
+          sx={{ color: 'red' }}
+        />
+      </Box>
+      <Box className="flex flex-row gap-12 overflow-x-auto no-scrollbar mt-3">
         {cardData.map((card, index) => (
           <TCard
             key={index}
