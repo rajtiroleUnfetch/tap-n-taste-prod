f496e3ce040d91e14ae4c1cc582d284062ef23f3
jayashreePedini <jayashree14work@gmail.com>
2024-12-11 17:40:36 +0530

    Add Bottom Info Pop Up of Menu Screen

--- t-scanning/src/app/pages/menu-page/menu-page.tsx
+++ t-scanning/src/app/pages/menu-page/menu-page.tsx
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
+import BottomInfoPopUp from './bottom-info-popup/bottom-info-popup';
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -56,6 +57,8 @@ const MenuPage = () => {
           ratings={3}
         />
       </Dialog>
+
+      <BottomInfoPopUp />
 
       <FooterPage />
     </Box>
